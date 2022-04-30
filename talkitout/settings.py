777bec48f742f4d361a49a60968080e22e87cc3a
Asmit Ganguly <92667287+asmit203@users.noreply.github.com>
2022-04-30 15:32:03 +0530

    setting update fix

--- talkitout/settings.py
+++ talkitout/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY ='django-insecure-kyh&g#a02(z*uw*s#2-q=9e=c%jd%d=lvmrd64dd$^%)7x4i#u'
+SECRET_KEY =''
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ["127.0.0.1",'localhost']
 ALLOWED_HOSTS = []
