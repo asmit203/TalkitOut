c5d2e47cf8ee657b1d3740ced671dbdf6a87a7e1
asmit203 <asmit.ganguly@gmail.com>
2022-12-07 13:18:14 +0530

    settings correction

--- talkitout/settings.py
+++ talkitout/settings.py
@@ -106,16 +106,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-DJRICHTEXTFIELD_CONFIG = {
-    'js': ['//cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js'],
-    'init_template': 'djrichtextfield/init/tinymce.js',
-    'settings': {
-        'menubar': False,
-        'plugins': 'link image',
-        'toolbar': 'bold italic | link image | removeformat',
-        'width': 700
-    }
-}
+# DJRICHTEXTFIELD_CONFIG = {
+#     'js': ['//cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js'],
+#     'init_template': 'djrichtextfield/init/tinymce.js',
+#     'settings': {
+#         'menubar': False,
+#         'plugins': 'link image',
+#         'toolbar': 'bold italic | link image | removeformat',
+#         'width': 700
+#     }
+# }
 
 
 # Internationalization
