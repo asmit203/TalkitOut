ca3fa7c599092a79f2c2b75054b285b1f40871de
HemantKumar01 <ytbhemant@gmail.com>
2023-12-10 22:36:37 +0530

    fixed a bug. added invite collaborators button, added link to navbar

--- TalkitOut-Backend/whiteboardcollab/views.py
+++ TalkitOut-Backend/whiteboardcollab/views.py
@@ -4,5 +4,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, "whiteboardcollab/index.html")
 
-def room(request, room_name):
+
+def whiteboard_room(request, room_name):
     return render(request, "whiteboardcollab/room.html", {"room_name": room_name})
