8c741d50d3d923ca495c5de38533d8bcdca4854a
Asmit Ganguly <asmit.ganguly@gmail.com>
2023-12-10 23:41:17 +0530

    going back

--- TalkitOut-Backend/blog/views.py
+++ TalkitOut-Backend/blog/views.py
@@ -1,6 +1,6 @@
 from re import template
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse,HttpResponseRedirect
 from .models import post, announcements, Friend, Group, Comment
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -10,55 +10,42 @@ from django.views.generic import (
     DetailView,
     CreateView,
     UpdateView,
-    DeleteView,
+    DeleteView
 )
 from django.contrib.auth.models import User
 from .forms import announcement_form, CreateGroupForm
 from users.models import Profile
 from django.utils import timezone
 
-
 def home(request):
     context = {
-        "posts": post.objects.all(),
+        'posts': post.objects.all(),
     }
     if request.user.is_authenticated:
         request.user.profile.lastseen = timezone.now()
         request.user.profile.save()
         current_user = request.user
-        friends = Friend.objects.filter(user=current_user).values_list(
-            "friend", flat=True
-        )
+        friends = Friend.objects.filter(user=current_user).values_list('friend', flat=True)
         others = User.objects.exclude(pk=current_user.id).exclude(pk__in=friends)
-        groups = Group.objects.filter(
-            members=current_user
-        )  # Get groups the user is a part of
-        context["friends"] = Friend.objects.filter(user=current_user)
-        context["others_list"] = others
-        context["groups"] = groups  # Add groups to the context
-        friends_lastseen = Profile.objects.filter(
-            user__in=friends.values_list("friend", flat=True)
-        ).values("user__username", "lastseen")
-        context["friends_lastseen"] = friends_lastseen
+        groups = Group.objects.filter(members=current_user)  # Get groups the user is a part of
+        context['friends'] = Friend.objects.filter(user=current_user)
+        context['others_list'] = others
+        context['groups'] = groups  # Add groups to the context
+        friends_lastseen = Profile.objects.filter(user__in=friends.values_list('friend', flat=True)).values('user__username', 'lastseen')
+        context['friends_lastseen'] = friends_lastseen
     print(context)
-
-    return render(request, "blog/home.html", context)
-
+    
+    return render(request, 'blog/home.html', context)
 
 def add_friend(request, username):
     request.user.profile.lastseen = timezone.now()
     request.user.profile.save()
     if request.user.is_authenticated:
         current_user = request.user
-        new_friend = User.objects.get(username=username)
+        new_friend = User.objects.get(username=username) 
         Friend.make_friendship(current_user, new_friend)
-        room_name = (
-            min(current_user.username, new_friend.username)
-            + "_"
-            + max(current_user.username, new_friend.username)
-        )
-        return redirect("room", room_name=room_name)
-
+        room_name = min(current_user.username, new_friend.username) + '_' + max(current_user.username, new_friend.username)
+        return redirect('room', room_name=room_name)
 
 # class PostListViews(ListView):
 #     model = post
@@ -66,83 +53,73 @@ def add_friend(request, username):
 #     context_object_name = 'posts'
 #     ordering = ['-date_posted']
 #     paginate_by = 5
-
-
+    
 class UserPostListViews(ListView):
     model = post
-    template_name = "blog/User_posts.html"
-    context_object_name = "posts"
-    ordering = ["-date_posted"]
+    template_name = 'blog/User_posts.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
     paginate_by = 5
-
-    # <app>/<model>_<viewtype>.html
+    #<app>/<model>_<viewtype>.html
     def get_queryset(self):
-        user = get_object_or_404(User, username=self.kwargs.get("username"))
-        return post.objects.filter(author=user).order_by("-date_posted")
-
+        user = get_object_or_404(User,username=self.kwargs.get('username'))
+        return post.objects.filter(author = user).order_by('-date_posted')
 
 class PostDetailsView(DetailView):
     model = post
-
     # template_name = 'blog/your_template_name.html'
-    def get_context_data(self, **kwargs):
+    def get_context_data(self,**kwargs):
         data = super().get_context_data(**kwargs)
 
-        post_instance = get_object_or_404(post, id=self.kwargs["pk"])
+        post_instance = get_object_or_404(post, id=self.kwargs['pk'])
 
-        votes_connected = get_object_or_404(post, id=self.kwargs["pk"])
+        votes_connected = get_object_or_404(post, id=self.kwargs['pk'])
         voted = False
         if votes_connected.votes.filter(id=self.request.user.id).exists():
             voted = True
-        data["number_of_votes"] = votes_connected.number_of_votes()
-        data["post_is_voted"] = voted
+        data['number_of_votes'] = votes_connected.number_of_votes()
+        data['post_is_voted'] = voted
 
         # Commenting logic
         comments = post_instance.comments.all()
-        data["comments"] = comments
+        data['comments'] = comments
         return data
 
-
-def PostVote(request, pk):
-    Post = get_object_or_404(post, id=request.POST.get("post_id"))
+def PostVote(request,pk):
+    Post=get_object_or_404(post,id=request.POST.get('post_id'))
     if Post.votes.filter(id=request.user.id).exists():
         Post.votes.remove(request.user)
     else:
         Post.votes.add(request.user)
-    return HttpResponseRedirect(reverse("post-detail", args=[str(pk)]))
-
+    return HttpResponseRedirect(reverse('post-detail', args=[str(pk)]))
 
 class CommentCreateView(CreateView):
     model = Comment
-    template_name = "blog/comment_form.html"  # Create a comment form template
-    fields = ["text"]
+    template_name = 'blog/comment_form.html'  # Create a comment form template
+    fields = ['text']
 
     def form_valid(self, form):
-        form.instance.post = get_object_or_404(post, id=self.kwargs["pk"])
+        form.instance.post = get_object_or_404(post, id=self.kwargs['pk'])
         form.instance.author = self.request.user
         return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse("post-detail", args=[str(self.kwargs["pk"])])
+        return reverse('post-detail', args=[str(self.kwargs['pk'])])
 
 
-class PostCreateView(LoginRequiredMixin, CreateView):
+class PostCreateView(LoginRequiredMixin,CreateView):
     model = post
-    fields = ["title", "content"]
-
-    def form_valid(self, form):
+    fields = ['title','content']
+    def form_valid(self,form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
     model = post
-    fields = ["title", "content"]
-
-    def form_valid(self, form):
+    fields = ['title','content']
+    def form_valid(self,form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-
     def test_func(self):
         post = self.get_object()
         if self.request.user == post.author:
@@ -150,159 +127,129 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         else:
             return False
 
-
-class PostDeleteView(
-    LoginRequiredMixin, UserPassesTestMixin, DeleteView
-):  # left of anything will be mixins
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):  #left of anything will be mixins
     model = post
     success_url = "/"
-
     def test_func(self):
         post = self.get_object()
         if self.request.user == post.author:
             return True
         else:
             return False
-
-
+        
 def about(request):
     request.user.profile.lastseen = timezone.now()
     request.user.profile.save()
-    context = {"title": "about"}
+    context = {'title':'about'}
     if request.user.is_authenticated:
         request.user.profile.lastseen = timezone.now()
         request.user.profile.save()
         current_user = request.user
-        friends = Friend.objects.filter(user=current_user).values_list(
-            "friend", flat=True
-        )
+        friends = Friend.objects.filter(user=current_user).values_list('friend', flat=True)
         others = User.objects.exclude(pk=current_user.id).exclude(pk__in=friends)
-        groups = Group.objects.filter(
-            members=current_user
-        )  # Get groups the user is a part of
-        context["friends"] = Friend.objects.filter(user=current_user)
-        context["others_list"] = others
-        context["groups"] = groups  # Add groups to the context
-        friends_lastseen = Profile.objects.filter(
-            user__in=friends.values_list("friend", flat=True)
-        ).values("user__username", "lastseen")
-        context["friends_lastseen"] = friends_lastseen
-    return render(request, "blog/about.html", context)
+        groups = Group.objects.filter(members=current_user)  # Get groups the user is a part of
+        context['friends'] = Friend.objects.filter(user=current_user)
+        context['others_list'] = others
+        context['groups'] = groups  # Add groups to the context
+        friends_lastseen = Profile.objects.filter(user__in=friends.values_list('friend', flat=True)).values('user__username', 'lastseen')
+        context['friends_lastseen'] = friends_lastseen
+    return render(request, 'blog/about.html', context)
 
 
 class UpVotedPostListViews(ListView):
     model = post
-    template_name = "blog/upvotedposts.html"
-    context_object_name = "posts"
-    ordering = ["-votes"]
+    template_name = 'blog/upvotedposts.html'
+    context_object_name = 'posts'
+    ordering = ['-votes']
     paginate_by = 5
 
 
-def favourite_add(request, id):
+def favourite_add(request,id):
     request.user.profile.lastseen = timezone.now()
     request.user.profile.save()
-    Post = get_object_or_404(post, id=id)
+    Post=get_object_or_404(post,id=id)
     if Post.favourites.filter(id=request.user.id).exists():
         Post.favourites.remove(request.user)
 
     else:
         Post.favourites.add(request.user)
 
-    return HttpResponseRedirect(request.META["HTTP_REFERER"])
-
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
 def favourite_list(request):
     request.user.profile.lastseen = timezone.now()
     request.user.profile.save()
-    new = post.objects.filter(favourites=request.user)
-    context = {"new": new}
+    new=post.objects.filter(favourites=request.user)
+    context = {'new':new}
     if request.user.is_authenticated:
         request.user.profile.lastseen = timezone.now()
         request.user.profile.save()
         current_user = request.user
-        friends = Friend.objects.filter(user=current_user).values_list(
-            "friend", flat=True
-        )
+        friends = Friend.objects.filter(user=current_user).values_list('friend', flat=True)
         others = User.objects.exclude(pk=current_user.id).exclude(pk__in=friends)
-        groups = Group.objects.filter(
-            members=current_user
-        )  # Get groups the user is a part of
-        context["friends"] = Friend.objects.filter(user=current_user)
-        context["others_list"] = others
-        context["groups"] = groups  # Add groups to the context
-        friends_lastseen = Profile.objects.filter(
-            user__in=friends.values_list("friend", flat=True)
-        ).values("user__username", "lastseen")
-        context["friends_lastseen"] = friends_lastseen
-    return render(request, "blog/favourites.html", context)
-
+        groups = Group.objects.filter(members=current_user)  # Get groups the user is a part of
+        context['friends'] = Friend.objects.filter(user=current_user)
+        context['others_list'] = others
+        context['groups'] = groups  # Add groups to the context
+        friends_lastseen = Profile.objects.filter(user__in=friends.values_list('friend', flat=True)).values('user__username', 'lastseen')
+        context['friends_lastseen'] = friends_lastseen
+    return render(request,'blog/favourites.html', context)
 
 def Announce(request):
     request.user.profile.lastseen = timezone.now()
     request.user.profile.save()
-    form = announcement_form()
-
-    allAnnouncements = announcements.objects.all()
-
+    form=announcement_form()
+    
+    allAnnouncements=announcements.objects.all()
+    
     user_detail = request.user
-    context = {
-        "announcements": allAnnouncements,
-        "user_detail": user_detail,
-        "form": form,
-    }
+    context={'announcements':allAnnouncements,'user_detail':user_detail,'form':form}
 
     if request.method == "POST":
         # title=request.POST['title']
         # announces=request.POST['announce']
         # announcements.objects.create(title=title,announce=announces)
         # allAnnouncements=announcements.objects.all()
-
+    
         # user_detail = request.user
         # context={'announcements':allAnnouncements,'user_detail':user_detail,'form':form}
-        form = announcement_form(request.POST)
+        form=announcement_form(request.POST)
         if form.is_valid():
             form.save()
-            return render(request, "blog/announcement.html", context)
-    return render(request, "blog/announcement.html", context)
-
+            return render(request,'blog/announcement.html',context)
+    return render(request,'blog/announcement.html',context)
 
 def create_group_page(request):
     request.user.profile.lastseen = timezone.now()
     request.user.profile.save()
     current_user = request.user
 
-    context = {"users": User.objects.exclude(pk=current_user.id)}
+    context = {'users': User.objects.exclude(pk=current_user.id)}
     if request.user.is_authenticated:
         request.user.profile.lastseen = timezone.now()
         request.user.profile.save()
-        friends = Friend.objects.filter(user=current_user).values_list(
-            "friend", flat=True
-        )
+        friends = Friend.objects.filter(user=current_user).values_list('friend', flat=True)
         others = User.objects.exclude(pk=current_user.id).exclude(pk__in=friends)
-        groups = Group.objects.filter(
-            members=current_user
-        )  # Get groups the user is a part of
-        context["friends"] = Friend.objects.filter(user=current_user)
-        context["others_list"] = others
-        context["groups"] = groups  # Add groups to the context
-        friends_lastseen = Profile.objects.filter(
-            user__in=friends.values_list("friend", flat=True)
-        ).values("user__username", "lastseen")
-        context["friends_lastseen"] = friends_lastseen
+        groups = Group.objects.filter(members=current_user)  # Get groups the user is a part of
+        context['friends'] = Friend.objects.filter(user=current_user)
+        context['others_list'] = others
+        context['groups'] = groups  # Add groups to the context
+        friends_lastseen = Profile.objects.filter(user__in=friends.values_list('friend', flat=True)).values('user__username', 'lastseen')
+        context['friends_lastseen'] = friends_lastseen
     print(context)
-    if request.method == "POST":
+    if request.method == 'POST':
         form = CreateGroupForm(request.POST)
         if form.is_valid():
-            group_name = form.cleaned_data["group_name"]
-            members = list(form.cleaned_data["members"])
+            group_name = form.cleaned_data['group_name']
+            members = list(form.cleaned_data['members'])
             members.append(current_user)
             # Create the group
             new_group = Group.objects.create(name=group_name)
             new_group.members.set(members)
 
-            return redirect("blog-home")
+            return redirect('blog-home')
     else:
         form = CreateGroupForm()
 
-    context["form"] = form
-    return render(request, "blog/create_group.html", context)
+    context['form'] = form
+    return render(request, 'blog/create_group.html', context)
--- TalkitOut-Backend/talkitout/settings.py
+++ TalkitOut-Backend/talkitout/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-
 # import dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,76 +21,77 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-kyh&g#a02(z*uw*s#2-q=9e=c%jd%d=lvmrd64dd$^%)7x4i#u"
+SECRET_KEY ='django-insecure-kyh&g#a02(z*uw*s#2-q=9e=c%jd%d=lvmrd64dd$^%)7x4i#u'
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
 DEBUG = True
 
 # ALLOWED_HOSTS = ["127.0.0.1",'localhost']
-ALLOWED_HOSTS = ["*"]  #!for allowing all the incoming connections
-# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"] #!for allowing all the incoming connections
+#ALLOWED_HOSTS = []
 # if not DEBUG:
 #     ALLOWED_HOSTS+=[os.environ.get('ALLOWED_HOST')]
 
 # Application definition
 
 INSTALLED_APPS = [
-    "daphne",
-    "whiteboardcollab",
-    "blog.apps.BlogConfig",
-    "users.apps.UsersConfig",
-    "chat.apps.ChatConfig",
-    "stream.apps.StreamConfig",
-    "crispy_forms",
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "ckeditor",
-    "crispy_bootstrap4",
-    "channels",
+    'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    'chat.apps.ChatConfig',
+    'stream.apps.StreamConfig',
+    'crispy_forms',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'ckeditor',
+    'crispy_bootstrap4',
+    'channels',
+    
 ]
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = "talkitout.urls"
+ROOT_URLCONF = 'talkitout.urls'
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = "talkitout.wsgi.application"
+WSGI_APPLICATION = 'talkitout.wsgi.application'
+
+
 
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -101,16 +101,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
@@ -129,9 +129,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = "Asia/Calcutta"
+TIME_ZONE = 'Asia/Calcutta'
 
 USE_I18N = True
 
@@ -141,36 +141,34 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "/static/"
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-CRISPY_TEMPLATE_PACK = "bootstrap4"
-LOGIN_REDIRECT_URL = "blog-home"
-LOGIN_URL = "login"
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = "587"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get("EMAIL_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASS")
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 CKEDITOR_CONFIGS = {
-    "default": {
-        "height": "full",
-        "width": "100%",
+    'default': {
+        'height': 'full', 
+        'width': '100%', 
     },
 }
-
-# Daphne
 ASGI_APPLICATION = "talkitout.asgi.application"
 CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
-    },
+    'default': {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
 }
