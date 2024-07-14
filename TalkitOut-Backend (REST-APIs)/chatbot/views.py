d59185c22e2518d789430a252b172b8606632414
Asmit Ganguly <asmit.ganguly@gmail.com>
2024-07-14 12:42:39 -0700

    added AI chatbot

--- TalkitOut-Backend (REST-APIs)/chatbot/views.py
+++ TalkitOut-Backend (REST-APIs)/chatbot/views.py
@@ -1,14 +1,25 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+import ollama
+
+def get_completion(prompt, model):
+    response = ollama.chat(model, messages=[{
+        'role': 'user',
+        'content': prompt,
+    }])
+
+    return response['message']['content']
+# print(get_completion(prompt, "phi"))
 
 
 @api_view(["POST"])
 def chatbotResponse(request):
     if request.method == "POST":
         data = request.data
-        print(data)
-        return Response("Hello!", status=status.HTTP_200_OK)
+        # print(data)
+        Resp = get_completion(data['query'], "phi3:latest")
+        return Response(Resp, status=status.HTTP_200_OK)
     else:
         return Response(
             {"message": "Method not allowed"}, status=status.HTTP_405_METHOD_NOT_ALLOWED
